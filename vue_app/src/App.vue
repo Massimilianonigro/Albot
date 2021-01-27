42e1d9c21bb66b67c867e29c50e1eb6b1b77bd9f
Leo Gomez <dalgomz@hotmail.com>
2021-01-27 01:52:15 -0500

    Frontend Socket Working

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,65 +1,69 @@
 <template>
-  <div id="app" :style="{ 'height': '100%'}">
-    <div v-if="mainStatus==0">
-      <MainScreen v-on:startIntro="startIntroduction" v-on:startPractice="startPractice"/>
+  <div id="app" :style="{ height: '100%' }">
+    <div v-if="mainStatus == 0">
+      <MainScreen
+        v-on:startIntro="startIntroduction"
+        v-on:startPractice="startPractice"
+      />
     </div>
     <div v-else>
-      <GameScreen
-        v-bind:gameType=mainStatus
-        v-on:goHome="resetHome"
-      />
-      <Chat/>
+      <GameScreen v-bind:gameType="mainStatus" v-on:goHome="resetHome" />
+      <Chat />
     </div>
+    <button class="button-exe" v-on:click="sendMessage('message test')">Send Message</button>
   </div>
 </template>
 
 <script>
-  // const io = require('socket.io-client')
-  import MainScreen from './components/MainScreen.vue'
-  import GameScreen from './components/GameScreen.vue'
-  import Chat from './components/Chat.vue'
-  
+// const io = require('socket.io-client')
+import MainScreen from "./components/MainScreen.vue";
+import GameScreen from "./components/GameScreen.vue";
+import Chat from "./components/Chat.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     MainScreen,
     GameScreen,
-    Chat
+    Chat,
   },
   data() {
     return {
-      message : "",
-      mainStatus: 0,
-      /*
-      socket: io('ws://localhost:2345', {
-        transports : ['websocket']
-      })
-      */
+      message: "",
+      mainStatus: 0
+    };
+  },
+  methods: {
+    resetHome() {
+      this.mainStatus = 0;
+    },
+    startIntroduction() {
+      this.mainStatus = 1;
+    },
+    startPractice() {
+      this.mainStatus = 3;
+    },
+    sendMessage: function(message) {
+      console.log(this.connection);
+      this.connection.send(message);
     }
   },
-/*
-  mounted() {
-    console.log(this.socket)
-    this.socket.on("MESSAGE", (socket) => {
-      console.log(socket);
-    })
+  created: function () {
+    console.log("Starting connection to WebSocket Server");
+    this.connection = new WebSocket("ws://localhost:2345");
 
+    
+
+    this.connection.onmessage = function (event) {
+      console.log(event);
+    };
+
+    this.connection.onopen = function (event) {
+      console.log(event);
+      console.log("Successfully connected to the echo websocket server...");
+    };
   },
-*/
-  methods: {
-    resetHome(){
-      this.mainStatus = 0
-    },
-    startIntroduction(){
-      this.mainStatus = 1
-    },
-    startPractice(){
-      this.mainStatus = 3
-    },
-  }
-
-}
+};
 </script>
 
 <style>
@@ -68,7 +72,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #828e99;
+}
+.button-exe{
+  position: absolute;
+  z-index: 100;
+  top: 105%;
 }
 </style>
 
